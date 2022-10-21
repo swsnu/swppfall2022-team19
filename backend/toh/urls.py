711d3d8d5f857fa0102f71dab99c2b746945a35d
jaewooMaeng <maeng9584@likelion.org>
2022-10-21 16:59:12 +0900

    fix urls

--- backend/toh/urls.py
+++ backend/toh/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('hero/', include('hero.urls')),
+    path('api/hero/', include('hero.urls')),
     path('admin/', admin.site.urls),
 ]
