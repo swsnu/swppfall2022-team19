936233b23265ec2e3f5da8d69774ed37cea84fa8
yunwkgus <52576276+yunwkgus@users.noreply.github.com>
2022-11-04 23:40:09 +0900

    Delete backend directory

--- backend/hero/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'), 
-    path('info/', views.hero_list, name='hero_list'),
-    path('info/<int:id>/', views.hero_info, name='hero_info')
-]
