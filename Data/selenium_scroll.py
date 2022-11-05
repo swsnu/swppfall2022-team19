dea8afc7a046300dca65bf54dee180d0dadfefff
Josewise <yunwkgus@naver.com>
2022-11-05 19:59:39 +0900

    data_jh

--- Data/selenium_scroll.py
+++ Data/selenium_scroll.py
@@ -15,7 +15,11 @@ category_name = ["convenient", "snack", "icecream", "food", "drink"]
 category_id = [1,3,4,5,6]
 # category_id = [5]
 
-for i in category_id:
+index = 0
+prod_list = []
+
+
+for cIndex, i in enumerate(category_id):
     url_link = "http://cu.bgfretail.com/product/product.do?category=product&depth2=4&depth3={}".format(i)
     
     driver.get(url_link)
@@ -33,7 +37,7 @@ for i in category_id:
 
     prod_itemAll = soup.find_all("a", "prod_item")
 
-    prod_list = []
+
 
     for prod in prod_itemAll:
         new_tag = prod.find("span", "new")
@@ -47,15 +51,16 @@ for i in category_id:
         prod_price = prod.find("div", "price").getText()
         prod_price = prod_price[:len(prod_price)-1].replace("," , "")
 
-        prod_dictionary = {"Name": prod_name, "Img": "https:"+prod_img["src"], "Price": int(prod_price)}
+        prod_dictionary = {"ID": index, "CategoryID": cIndex, "Name": prod_name, "Img": "https:"+prod_img["src"], "Price": int(prod_price)}
+        index = index + 1 
         prod_list.append(prod_dictionary)
 
     print(prod_list)
 
-    if i == 1:
-        file_name = "/Users/jahyunyun/Desktop/2022-2/소프트웨어 개발의 원리와 실재/PROJECT/Product_data_{}.json".format(category_name[i-1])
-    else: 
-        file_name = "/Users/jahyunyun/Desktop/2022-2/소프트웨어 개발의 원리와 실재/PROJECT/Product_data_{}.json".format(category_name[i-2])
     
-    with open(file_name, "w") as outfile:
-        json.dump(prod_list, outfile, ensure_ascii=False, indent=2)
+    
+    
+file_name = "/Users/jahyunyun/Desktop/2022-2/소프트웨어 개발의 원리와 실재/TeamRepo/swppfall2022-team19/Data/product_data.json"    
+
+with open(file_name, "w") as outfile:
+    json.dump(prod_list, outfile, ensure_ascii=False, indent=2)
