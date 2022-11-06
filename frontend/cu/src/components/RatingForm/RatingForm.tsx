8847775d2424e350e9ed1af6837a7b661385d590
hanaisreal <okd2000@gmail.com>
2022-11-06 12:41:01 +0900

    rate form update

--- frontend/cu/src/components/RatingForm/RatingForm.tsx
+++ frontend/cu/src/components/RatingForm/RatingForm.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+import { Navigate, useNavigate } from "react-router-dom";
 import HeartRating from './HeartRating'
+import "./RatingForm.css"
 
 type Props = {
   user_id: number,
@@ -13,6 +15,8 @@ type Props = {
 
 
 function RatingForm(props: Props) {
+  const navigate = useNavigate();
+  const [rateState, setRateState] = useState(false);
 
   //update score for each question when the user clicks rating
   const [score1, setScore1] = useState(0);
@@ -20,6 +24,7 @@ function RatingForm(props: Props) {
   const [score3, setScore3] = useState(0);
   const [score4, setScore4] = useState(0);
   const [score5, setScore5] = useState(0);
+  const [comment, setComment] = useState("");
 
   const updateScore1 = (score: number): void => {
     setScore1(score)
@@ -37,50 +42,82 @@ function RatingForm(props: Props) {
     setScore5(score)
   }
 
+  const clickRateHandler = () => {
+    setRateState(true);
+  }
+  const clickBackHandler = () =>{
+    setRateState(false);
+  }
+
+  const clickSaveHandler = () => {
+  }
+
   return (
-    <div className="ratingForm">
-      맛 만족도 {<HeartRating score={score1} updateScore={updateScore1} />}
-      가성비 {<HeartRating score={score2} updateScore={updateScore2} />}
-      재구매 의사 {<HeartRating score={score3} updateScore={updateScore3} />}
-
+    <div>
       <div>
-        {props.category_id === 0 &&
-          <div> 편리성 </div>
+        {rateState === false && 
+          <button id='rate_button' onClick={() => clickRateHandler()}>평가하기</button>
         }
-        {props.category_id === 1 &&
-          <div> 용량 </div>
-        }
-        {props.category_id === 2 &&
-          <div> 신선도 </div>
-        }
-        {props.category_id === 3 &&
-          <div> 건더기 양 </div>
-        }
-        {props.category_id === 4 &&
-          <div> 목넘김 </div>
-        }
-        {<HeartRating score={score4} updateScore={updateScore4} />}
       </div>
-
       <div>
-        {props.category_id === 0 &&
-          <div> 재료 양 </div>
+        {rateState === true &&
+              <div className="rating_form">
+              <h2 className="rating_heading"> 리뷰 작성하기</h2>
+              <button id='cancel_button' onClick={() => clickBackHandler()}>취소</button>
+              <button id='save_button' onClick={() => clickSaveHandler()}>저장</button>
+              <br></br>
+
+              <div>맛 만족도 {<HeartRating score={score1} updateScore={updateScore1} />} </div>
+              <div>가성비 {<HeartRating score={score2} updateScore={updateScore2} />}</div>
+              <div>재구매 의사 {<HeartRating score={score3} updateScore={updateScore3} />}</div>
+        
+              <div>
+                {props.category_id === 0 &&
+                  <div> 편리성 </div>
+                }
+                {props.category_id === 1 &&
+                  <div> 용량 </div>
+                }
+                {props.category_id === 2 &&
+                  <div> 신선도 </div>
+                }
+                {props.category_id === 3 &&
+                  <div> 건더기 양 </div>
+                }
+                {props.category_id === 4 &&
+                  <div> 목넘김 </div>
+                }
+                {<HeartRating score={score4} updateScore={updateScore4} />}
+              </div>
+        
+              <div>
+                {props.category_id === 0 &&
+                  <div> 재료 양 </div>
+                }
+                {props.category_id === 1 &&
+                  <div> 추천 의사 </div>
+                }
+                {props.category_id === 2 &&
+                  <div> 당도 </div>
+                }
+                {props.category_id === 3 &&
+                  <div> 간편함 </div>
+                }
+                {props.category_id === 4 &&
+                  <div> 당도 </div>
+                }
+                {<HeartRating score={score5} updateScore={updateScore5} />}
+              </div>
+
+              <div>
+                <div>한줄평</div>
+                <input type="text" onChange={(event) => setComment(event.target.value)}/>
+              </div>
+            </div>
         }
-        {props.category_id === 1 &&
-          <div> 추천 의사 </div>
-        }
-        {props.category_id === 2 &&
-          <div> 당도 </div>
-        }
-        {props.category_id === 3 &&
-          <div> 간편함 </div>
-        }
-        {props.category_id === 4 &&
-          <div> 당도 </div>
-        }
-        {<HeartRating score={score5} updateScore={updateScore5} />}
-      </div>
+        </div>
     </div>
+    
   )
 }
 
--- frontend/cu/src/store/slices/Review.ts
+++ frontend/cu/src/store/slices/Review.ts
@@ -8,7 +8,7 @@ export interface ReviewType{
     product_id: number, 
     category_id: number,
     score: number[],
-    //comment:string  --> 다른 컴포넌트에 만들어져 있어서 어떻게 integrate할 지 결정해야함
+    comment:string 
     }
 
 export interface ReviewState{
@@ -18,7 +18,7 @@ export interface ReviewState{
 
 const initialState: ReviewState = {
     reviews: [
-        {id: 1, user_id: 1, product_id: 1, category_id: 1, score: [4, 3, 5, 3, 3]}
+        {id: 1, user_id: 1, product_id: 1, category_id: 1, score: [4, 3, 5, 3, 3], comment: "조금 짜긴한데 만족합니다!"}
     ],
     selectedReview: null
 }
@@ -34,7 +34,7 @@ export const fetchReview = createAsyncThunk(
 //post review to specific product
 export const postReview = createAsyncThunk(
     "api/postReview",
-   async (review: Pick<ReviewType, "user_id"|"product_id"|"category_id"|"score">,{dispatch}) => {
+   async (review: Pick<ReviewType, "user_id"|"product_id"|"category_id"|"score"|"comment">,{dispatch}) => {
     const response = await axios.post("/api/productID/reviews/", review);
     dispatch(reviewActions.addReview(response.data));
    }
@@ -45,13 +45,14 @@ export const reviewSlice = createSlice({
     name: "review",
     initialState,
     reducers: {
-        addReview: (state, action: PayloadAction<{user_id: number, product_id: number, category_id:number,score: number[]}>) =>{
+        addReview: (state, action: PayloadAction<{user_id: number, product_id: number, category_id:number,score: number[], comment: string}>) =>{
             const newReview = {
                 id: state.reviews.length === 0 ? state.reviews.length + 1: state.reviews[state.reviews.length -1].id + 1,
                 user_id: action.payload.user_id,
                 product_id: action.payload.product_id,
                 category_id: action.payload.category_id,
                 score : action.payload.score,
+                comment : action.payload.comment
             }
             state.reviews.push(newReview);
             state.selectedReview = newReview;
