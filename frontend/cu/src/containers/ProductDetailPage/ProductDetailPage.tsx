4184c3fc3f1fe0abbdc8fc784edf71897ba6bf67
nyminna <112807101+nyminna@users.noreply.github.com>
2022-11-06 16:47:47 +0900

    update Product Detail Page
    add TotalScoreList, ReviewList

--- frontend/cu/src/containers/ProductDetailPage/ProductDetailPage.tsx
+++ frontend/cu/src/containers/ProductDetailPage/ProductDetailPage.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import RatingForm from '../../components/RatingForm/RatingForm'
+import React from 'react';
+import RatingForm from '../../components/RatingForm/RatingForm';
+import TotalScoreList from '../../components/TotalScoreList/TotalScoreList';
+import ReviewList from '../../components/ReviewList/ReviewList';
 
 function ProductDetailPage() {
-
 
     //왼편에 product, 오른편에 rating, 아래에 totalScoreList, 맨 아래에는 reviewList.
 
   return (
     <div>
       {<RatingForm user_id={1} product_id={1} category_id = {1} score={[]}/>}
+      {<TotalScoreList title={"전체 평점 및 항목별 평점"} />}
+      {<ReviewList title={"상품 후기"} />}
     </div>
   )
 }
