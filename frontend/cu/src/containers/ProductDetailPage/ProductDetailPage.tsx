0efbbc01b216df5665c343e7eea3822d3437bc5f
hanaisreal <okd2000@gmail.com>
2022-11-07 13:28:49 +0900

    add productBlock

--- frontend/cu/src/containers/ProductDetailPage/ProductDetailPage.tsx
+++ frontend/cu/src/containers/ProductDetailPage/ProductDetailPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import RatingForm from '../../components/RatingForm/RatingForm';
 import TotalScoreList from '../../components/TotalScoreList/TotalScoreList';
 import ReviewList from '../../components/ReviewList/ReviewList';
+import ProductBlock from '../../components/ProductBlock/ProductBlock';
 import "./ProductDetailPage.css"
 
 function ProductDetailPage() {
@@ -12,6 +13,7 @@ function ProductDetailPage() {
   return (
     <div>
       <div>
+        {<ProductBlock pageLink="/" imgLink="https://tqklhszfkvzk6518638.cdn.ntruss.com/product/8809140575360.jpg" name="맛있는 제품" price={3400}/>}
         {<RatingForm user_id={1} product_id={1} category_id={1} score={[]} />}
         {<TotalScoreList title={"전체 평점 및 항목별 평점"} />}
         {<ReviewList title={"상품 후기"} />}
