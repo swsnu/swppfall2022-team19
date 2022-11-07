ef40f529bbb4d5d40225f86492e29f2658b8802b
hanaisreal <okd2000@gmail.com>
2022-11-07 19:29:40 +0900

    fixed rating form after save

--- frontend/cu/src/components/RatingForm/RatingForm.tsx
+++ frontend/cu/src/components/RatingForm/RatingForm.tsx
@@ -6,6 +6,7 @@ import { postReview, reviewActions } from '../../store/slices/Review';
 import HeartRating from './HeartRating'
 import "./RatingForm.css"
 import { AppDispatch, RootState } from '../../store';
+import FixedHeartRating from './FixedHeartRating';
 
 type Props = {
   user_id: number,
@@ -28,7 +29,9 @@ function RatingForm(props: Props) {
   const [score4, setScore4] = useState(0);
   const [score5, setScore5] = useState(0);
   const [comment, setComment] = useState("");
-  const [rateState, setRateState] = useState(false);
+  const [rateState, setRateState] = useState(false);   //user에서 해당 제품을 평가했는지 확인 후 state에 넣을 예정 
+  const [clickRate, setClickRate] = useState(false);
+  const [hideButton, setHideButton] = useState(false);
 
   const updateScore1 = (score: number): void => {
     setScore1(score)
@@ -48,92 +51,149 @@ function RatingForm(props: Props) {
 
   const clickBackHandler = () => {
     setRateState(false);
+    setClickRate(false);
+    setHideButton(false);
   }
 
   const clickSaveHandler = async () => {
     const scores = [score1, score2, score3, score4, score5];
     const dataRate = { user_id: 1, product_id: 1, category_id: 1, scores: scores, comment: comment };
     const result1 = await dispatch(postRate(dataRate));
-    const dataReview = {user_id: 1, scores: scores, comment: comment, likedCount: 0, liked: false};
+
+    const dataReview = { user_id: 1, scores: scores, comment: comment, likedCount: 0, liked: false };
     const result2 = await dispatch(postReview(dataReview));
+    setRateState(true);
+    setClickRate(false);
 
-
-    clickBackHandler();
-    //   if(result.type === `${postRate.typePrefix}/fulfilled`){
-    // }
+    if (result1.type === `${postRate.typePrefix}/fulfilled`) {
+      //user가 남긴 평가 프로덕트 리스트에 추가해야함
+    }
 
   }
 
 
   const clickRateHandler = () => {
-    setRateState(true);
+    setClickRate(true);
+    setHideButton(true);
   }
 
   return (
     <div>
       <div className="rating_form">
         <div id='rating_blank'>
-          {rateState === false &&
-          <div className='rate_box'>
-          <button id="rate_button" hidden={rateState} onClick={() => clickRateHandler()}>평가하기</button>
-        </div>}
-        </div>
-        <div>
-          {rateState === true &&
-            <div>
-              <h2 className="rating_heading"> 리뷰 작성하기</h2>
-              <button id='cancel_button' onClick={() => clickBackHandler()}>취소</button>
-              <button id='save_button' onClick={() => clickSaveHandler()}>저장</button>
-              <br></br>
-
-              <div>맛 만족도 {<HeartRating score={score1} updateScore={updateScore1} />} </div>
-              <div>가성비 {<HeartRating score={score2} updateScore={updateScore2} />}</div>
-              <div>재구매 의사 {<HeartRating score={score3} updateScore={updateScore3} />}</div>
-
-              <div>
-                {props.category_id === 0 &&
-                  <div> 편리성 </div>
-                }
-                {props.category_id === 1 &&
-                  <div> 용량 </div>
-                }
-                {props.category_id === 2 &&
-                  <div> 신선도 </div>
-                }
-                {props.category_id === 3 &&
-                  <div> 건더기 양 </div>
-                }
-                {props.category_id === 4 &&
-                  <div> 목넘김 </div>
-                }
-                {<HeartRating score={score4} updateScore={updateScore4} />}
-              </div>
-
-              <div>
-                {props.category_id === 0 &&
-                  <div> 재료 양 </div>
-                }
-                {props.category_id === 1 &&
-                  <div> 추천 의사 </div>
-                }
-                {props.category_id === 2 &&
-                  <div> 당도 </div>
-                }
-                {props.category_id === 3 &&
-                  <div> 간편함 </div>
-                }
-                {props.category_id === 4 &&
-                  <div> 당도 </div>
-                }
-                {<HeartRating score={score5} updateScore={updateScore5} />}
-              </div>
-
-              <div>
-                <div>한줄평</div>
-                <input type="text" onChange={(event) => setComment(event.target.value)} />
-              </div>
+          {rateState === false && hideButton === false && 
+            <div className='rate_box'>
+              <button id="rate_button" hidden={hideButton} onClick={() => clickRateHandler()}>평가하기</button>
             </div>
-          }
+            }
+            {rateState === false && clickRate === true &&
+                  <div>
+                    <h2 className="rating_heading"> 리뷰 작성하기</h2>
+                    <button id='cancel_button' onClick={() => clickBackHandler()}>취소</button>
+                    <button id='save_button' onClick={() => clickSaveHandler()}>저장</button>
+                    <br></br>
+
+                    <div>맛 만족도 {<HeartRating score={score1} updateScore={updateScore1} />} </div>
+                    <div>가성비 {<HeartRating score={score2} updateScore={updateScore2} />}</div>
+                    <div>재구매 의사 {<HeartRating score={score3} updateScore={updateScore3} />}</div>
+
+                    <div>
+                      {props.category_id === 0 &&
+                        <div> 편리성 </div>
+                      }
+                      {props.category_id === 1 &&
+                        <div> 용량 </div>
+                      }
+                      {props.category_id === 2 &&
+                        <div> 신선도 </div>
+                      }
+                      {props.category_id === 3 &&
+                        <div> 건더기 양 </div>
+                      }
+                      {props.category_id === 4 &&
+                        <div> 목넘김 </div>
+                      }
+                      {<HeartRating score={score4} updateScore={updateScore4} />}
+                    </div>
+
+                    <div>
+                      {props.category_id === 0 &&
+                        <div> 재료 양 </div>
+                      }
+                      {props.category_id === 1 &&
+                        <div> 추천 의사 </div>
+                      }
+                      {props.category_id === 2 &&
+                        <div> 당도 </div>
+                      }
+                      {props.category_id === 3 &&
+                        <div> 간편함 </div>
+                      }
+                      {props.category_id === 4 &&
+                        <div> 당도 </div>
+                      }
+                      {<HeartRating score={score5} updateScore={updateScore5} />}
+                    </div>
+
+                    <div>
+                      <div>한줄평</div>
+                      <input type="text" onChange={(event) => setComment(event.target.value)} />
+                    </div>
+                  </div>
+                }
+        </div>
+        {rateState === true &&
+          <div>
+            <h2 className="rating_heading"> 내 리뷰</h2>
+            <div>맛 만족도 {<FixedHeartRating score={score1}/>} </div>
+                    <div>가성비 {<FixedHeartRating score={score2} />}</div>
+                    <div>재구매 의사 {<FixedHeartRating score={score3} />}</div>
+
+                    <div>
+                      {props.category_id === 0 &&
+                        <div> 편리성 </div>
+                      }
+                      {props.category_id === 1 &&
+                        <div> 용량 </div>
+                      }
+                      {props.category_id === 2 &&
+                        <div> 신선도 </div>
+                      }
+                      {props.category_id === 3 &&
+                        <div> 건더기 양 </div>
+                      }
+                      {props.category_id === 4 &&
+                        <div> 목넘김 </div>
+                      }
+                      {<FixedHeartRating score={score4}/>}
+                    </div>
+
+                    <div>
+                      {props.category_id === 0 &&
+                        <div> 재료 양 </div>
+                      }
+                      {props.category_id === 1 &&
+                        <div> 추천 의사 </div>
+                      }
+                      {props.category_id === 2 &&
+                        <div> 당도 </div>
+                      }
+                      {props.category_id === 3 &&
+                        <div> 간편함 </div>
+                      }
+                      {props.category_id === 4 &&
+                        <div> 당도 </div>
+                      }
+                      {<FixedHeartRating score={score5}/>}
+                    </div>
+                    <div>
+                      <div>한줄평</div>
+                      <text> {comment}</text>
+                    </div>
+          </div>
+        }
+
+        <div>
         </div>
       </div>
     </div>
--- frontend/cu/src/containers/ProductDetailPage/ProductDetailPage.tsx
+++ frontend/cu/src/containers/ProductDetailPage/ProductDetailPage.tsx
@@ -9,11 +9,11 @@ function ProductDetailPage() {
 
   //왼편에 product, 오른편에 rating, 아래에 totalScoreList, 맨 아래에는 reviewList.
 
-
   return (
     <div>
       <div>
-        {<ProductBlock pageLink="/" imgLink="https://tqklhszfkvzk6518638.cdn.ntruss.com/product/8809140575360.jpg" name="맛있는 제품" price={3400}/>}
+        <div></div>
+        {<ProductBlock pageLink="/" imgLink="https://tqklhszfkvzk6518638.cdn.ntruss.com/product/8809140575360.jpg" name="계란 샌드위치" price={3400}/>}
         {<RatingForm user_id={1} product_id={1} category_id={1} score={[]} />}
         {<TotalScoreList title={"전체 평점 및 항목별 평점"} />}
         {<ReviewList title={"상품 후기"} />}
