b5ce4d83cbc9a0aa02969dd3a1b5aba72435e48d
Josewise <yunwkgus@naver.com>
2022-11-07 13:38:51 +0900

    user_jh

--- frontend/cu/src/containers/UserList/Login.tsx
+++ frontend/cu/src/containers/UserList/Login.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { AppDispatch } from "../../store";
-import { putUser } from "../../store/slices/user";
+// import { putUser } from "../../store/slices/user";
 import "./Signup.css";
 
 export default function Login() {
@@ -12,15 +12,15 @@ export default function Login() {
   const [submitted, setSubmitted] = useState<boolean>(false);
   const dispatch = useDispatch<AppDispatch>();
 
-  const putUserHandler = async () => {
-    const data = { username: username, password: password };
-    const result = await dispatch(putUser(data));
-    if (result.type === `${putUser.typePrefix}/fulfilled`) {
-      setSubmitted(true);
-    } else {
-      alert("Error on login User");
-    }
-  };
+  // const putUserHandler = async () => {
+  //   const data = { username: username, password: password };
+  //   const result = await dispatch(putUser(data));
+  //   if (result.type === `${putUser.typePrefix}/fulfilled`) {
+  //     setSubmitted(true);
+  //   } else {
+  //     alert("Error on login User");
+  //   }
+  // };
 
   if (submitted) {
     return <Navigate to="/home" />; // to home, temp hero
@@ -42,7 +42,7 @@ export default function Login() {
           onChange={(event) => setPassword(event.target.value)}
         />
 
-        <button onClick={() => putUserHandler()}>Login</button>
+        {/* <button onClick={() => putUserHandler()}>Login</button> */}
       </div>
     );
   }
