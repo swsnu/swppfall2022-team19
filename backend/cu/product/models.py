4e5cf3fe218549cffa3b98f78f593839f39f237f
Josewise <yunwkgus@naver.com>
2022-11-13 02:29:27 +0900

    productDataAdded

--- backend/cu/product/models.py
+++ backend/cu/product/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length = 100)
+    #user subscribed to tags?? 
+
+
+class Product(models.Model):
+    name = models.CharField(max_length = 300, blank=False, null=False)
+    mainCategory = models.CharField(max_length = 100, blank=False, null=False)
+    subCategory = models.CharField(max_length = 300, blank=False, null=False)
+    imageUrl = models.URLField(blank = False)
+    details = models.TextField(max_length = 300, blank=False, null=False)
+    price = models.IntegerField(blank = False)
+    newProduct = models.BooleanField(default = False)
+    tags = models.ManyToManyField(Tag, related_name = "products")
+    averageScore = models.FloatField(default = 0)
+
+
+    def __str__(self):
+        return self.name
+
+
