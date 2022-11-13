3c6e4f09e4fa9c989ec70cdc54562632ff4adcc8
GAON25 <kim@DESKTOP-CBNUI9E.localdomain>
2022-11-13 21:08:25 +0900

    로그인 관련 백엔드 함수 작성

--- backend/cu/user/urls.py
+++ backend/cu/user/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # start login
-    # path('login/', views.user_info, name='login'),
-    # path('signup/', views.user_list, name='signup')
-    path('login/', views.signin, name='login'),
+    path('signin/', views.signin, name='login'),
     path('signout/', views.signout, name='signout'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('token/', views.token, name='token'),
+    path('requestUser/', views.requestUser, name="requestUser"),
+    path('userlist/', views.userlist, name="userlist"),
 ]
+
+# path('newSurvey/', views.changeSurvey, name="changeSurvey")
+# 추후 MyPage에서 Survey 정보 수정할 때 활용할 예정
