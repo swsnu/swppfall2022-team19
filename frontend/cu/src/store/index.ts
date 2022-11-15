858df7e9d079e1c10c8ef970f1ece38fca559a46
GAON25 <kim@DESKTOP-CBNUI9E.localdomain>
2022-11-15 21:42:11 +0900

    User Test

--- frontend/cu/src/store/index.ts
+++ frontend/cu/src/store/index.ts
@@ -2,15 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slices/product"
 import userReducer from "./slices/User";
 import rateReducer from "./slices/rate";
-import axios from "axios";
-import getCookie from "axios";
+import axios, getCookie from "axios";
 
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-
-// axios.defaults.xsrfCookieName = 'XSRF-TOKEN';
-// axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
+// 테스트 각주 axios.defaults.xsrfCookieName = 'XSRF-TOKEN';
+// 테스트 각주 axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
 
 export const store = configureStore({
     reducer: {
