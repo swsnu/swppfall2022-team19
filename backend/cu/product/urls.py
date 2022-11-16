18f20fef8496147e83bfb08bbdd70fd7e759c8a8
nyminna <lizzie00@snu.ac.kr>
2022-11-16 20:00:32 +0900

    product app update with pycache files delete

--- backend/cu/product/urls.py
+++ backend/cu/product/urls.py
@@ -8,26 +8,3 @@ router.register("product", ProductViewSet, basename="product")
 router.register("rate", RateViewSet, basename="rate")
 
 urlpatterns = [path("", include(router.urls))]
-
-
-
-'''
-# api/product/
-# total product -> list
-# specific product-> retrieve
-
-# api/rate/
-# total rate... -> list
-# specific rate -> retrieve, update, destroy
-
-urlpatterns = [
-    path('<str:mainCategory>/all/', ProductViewSet.list, name='product_by_main_category'),
-    path('<int:id>/', ProductViewSet.retrieve, name = "getProduct"),
-    path('<int:id>/', ProductViewSet.update, name = "updateProduct"),
-    path('<int:id>/rate/' ,RateViewSet.create, name='createRate'),
-    path('<int:id>/rates/', RateViewSet.list, name='fetchRates'),
-    path('rate/<int:id>/', RateViewSet.update, name='updateRate'),
-    path('rate/<int:id>/', RateViewSet.destroy, name='deleteRate'),
-]
-'''
-
