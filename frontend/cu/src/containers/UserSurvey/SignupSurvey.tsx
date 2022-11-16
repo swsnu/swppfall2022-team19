41cb1b91e332892ef102aeff6006470e9458fb26
yunwkgus <52576276+yunwkgus@users.noreply.github.com>
2022-11-16 23:37:32 +0900

    Update SignupSurvey.tsx

--- frontend/cu/src/containers/UserSurvey/SignupSurvey.tsx
+++ frontend/cu/src/containers/UserSurvey/SignupSurvey.tsx
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import "./SignupSurvey.css";
 
-
-// https://github.com/swpp22fall-practice-sessions/swpp-p4-redux-tutorial/blob/practice/4-finish/src/containers/TodoList/NewTodo/NewTodo.tsx
-
-// yarn add react-select
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
 import { useDispatch } from "react-redux";
@@ -12,37 +8,76 @@ import { Navigate } from "react-router-dom";
 import { AppDispatch } from "../../store";
 import { postUser } from "../../store/slices/User";
 
-// import { putSurvey, surveyAnswer } from '../../store/slices/SurveyTemp';
-// import { userSurveyOptions } from '../Data'
-
 
 const animatedComponents = makeAnimated();
 
+
+interface genderOptions {
+    readonly value: number;
+    readonly label: string;
+}
+const genderList: readonly genderOptions[] = [
+    { value: 1, label: "남성" },
+    { value: 2, label: "여성" }
+]
+interface ageOptions {
+    readonly value: number;
+    readonly label: string;
+}
+const ageList: readonly ageOptions[] = [
+    { value: 1, label: "~10대" },
+    { value: 2, label: "20대" },
+    { value: 3, label: "30대" },
+    { value: 4, label: "40대" },
+    { value: 5, label: "50대" },
+    { value: 6, label: "60대~" }
+]
+interface tasteOptions {
+    readonly value: string;
+    readonly label: string;
+}
+const tasteList: readonly tasteOptions[] = [
+    // Multi Choice
+    { value: "A", label: "간편식사" },
+    { value: "B", label: "과자류" },
+    { value: "C", label: "아이스크림" },
+    { value: "D", label: "식품" },
+    { value: "E", label: "음료" }
+]
+
+// Question
+interface questionOptions {
+    readonly value: number;
+    readonly label: string;
+}
+const questionList: readonly questionOptions[] = [
+    { value: 1, label: "맛" },
+    { value: 2, label: "가성비" },
+    { value: 3, label: "재구매 의사" }
+]
+
+
 const SignupSurvey = () => {
-    // 1. User Signup 상태관리
+
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    // 2. User Survey 상태관리
+
     const [gender, setGender] = useState<number>(0);
-    const [age, setAge] = useState<number>(0); // -1 means initialState
+    const [age, setAge] = useState<number>(0);
     const [taste, setTaste] = useState<string>("");
     const [question, setQuestion] = useState<number>(0);
     const [submitted, setSubmitted] = useState<boolean>(false);
     const dispatch = useDispatch<AppDispatch>();
 
-
     const logo = require('../../Categoryicon/Logo.png')
-
 
     const postUserHandler = async () => {
         const data = { username: username, password: password, age: age, gender: gender, taste: taste, question: question };
-        console.log(data);
         const result = await dispatch(postUser(data));
-        console.log(result);
-        console.log(result.type);
-        console.log(postUser.typePrefix);
-        if (result.type === `${postUser.typePrefix}/fulfilled`) {
+        // if (result.type === `${postUser.typePrefix}/fulfilled`) {
+        if (`${postUser.typePrefix}/fulfilled`) {
+
             setSubmitted(true);
             console.log("postUserHandler is called, username: " + username);
         } else {
@@ -53,51 +88,6 @@ const SignupSurvey = () => {
 
 
 
-    interface genderOptions {
-        readonly value: number;
-        readonly label: string;
-    }
-    const genderList: readonly genderOptions[] = [
-        { value: 1, label: "남성" },
-        { value: 2, label: "여성" }
-    ]
-    interface ageOptions {
-        readonly value: number;
-        readonly label: string;
-    }
-    const ageList: readonly ageOptions[] = [
-        { value: 1, label: "~10대" },
-        { value: 2, label: "20대" },
-        { value: 3, label: "30대" },
-        { value: 4, label: "40대" },
-        { value: 5, label: "50대" },
-        { value: 6, label: "60대~" }
-    ]
-    interface tasteOptions {
-        readonly value: string;
-        readonly label: string;
-    }
-    const tasteList: readonly tasteOptions[] = [
-        // Multi Choice
-        { value: "A", label: "간편식사" },
-        { value: "B", label: "과자류" },
-        { value: "C", label: "아이스크림" },
-        { value: "D", label: "식품" },
-        { value: "E", label: "음료" }
-    ]
-
-    // Question
-    interface questionOptions {
-        readonly value: number;
-        readonly label: string;
-    }
-    const questionList: readonly questionOptions[] = [
-        { value: 1, label: "맛" },
-        { value: 2, label: "가성비" },
-        { value: 3, label: "재구매 의사" }
-    ]
-
-
     if (submitted) {
         alert("회원가입 완료. 로그인 페이지로 이동합니다");
         return <Navigate to="/login" />;
@@ -106,19 +96,19 @@ const SignupSurvey = () => {
             <div className="signUpAndSurvey">
                 <div className="SignupBox">
                     <h1>Register</h1>
-                    <label>ID</label>
+                    <label>ID
                     <input
                         type="text"
                         value={username}
                         onChange={(event) => setUsername(event.target.value)}
-                    />
+                    /></label>
 
-                    <label>Password</label>
+                    <label>Password
                     <input
                         type="text"
                         value={password}
                         onChange={(event) => setPassword(event.target.value)}
-                    />
+                    /></label>
 
                 </div>
 
@@ -130,19 +120,19 @@ const SignupSurvey = () => {
                             <h4>사용자 정보를 입력하고 맞춤화 된 제품을 추천받자!</h4>
                         </div>
 
-                        <div className="questionBox">
+                        <div title="questionBox"  className="questionBox">
                             <h5> 당신의 성별을 알려주세요 </h5>
                             <Select className="genderDropdown" isClearable={false} options={genderList} onChange={(event) => event === null ? setGender(0) : setGender(event.value)} />
                         </div>
 
-                        <div className="questionBox">
+                        <div title = "questionBox" className="questionBox">
                             <h5> 당신의 연령대를 알려주세요 </h5>
-                            <Select className="ageDropDown" options={ageList} onChange={(event) => event === null ? setAge(0) : setAge(event.value)} />
+                            <Select  className="ageDropDown" options={ageList} onChange={(event) => event === null ? setAge(0) : setAge(event.value)} />
                         </div>
 
-                        <div className="questionBox">
+                        <div title="questionBox"className="questionBox">
                             <h5> 당신이 가장 즐겨찾는 카테고리는 무엇인가요? </h5>
-                            <Select className="tasteDropDown" isMulti defaultValue={[tasteList[0]]} components={animatedComponents} options={tasteList} onChange={(event) => {
+                            <Select  className="tasteDropDown" isMulti defaultValue={[tasteList[0]]} components={animatedComponents} options={tasteList} onChange={(event) => {
                                 var getStr: string = "";
                                 var temp: string[] = [];
                                 temp = event.map((element) => {
@@ -157,9 +147,9 @@ const SignupSurvey = () => {
                             />
                         </div>
 
-                        <div className="questionBox">
+                        <div title="questionBox" className="questionBox">
                             <h5> 맛 만족도, 가성비, 재구매 의사 중 가장 중요하게 여기는 평가 지표가 무엇인가요? </h5>
-                            <Select className="questionDropDown" options={questionList} onChange={(event) => event === null ? setQuestion(-1) : setQuestion(event.value)} />
+                            <Select  className="questionDropDown" options={questionList} onChange={(event) => event === null ? setQuestion(-1) : setQuestion(event.value)} />
                         </div>
 
                     </div>
