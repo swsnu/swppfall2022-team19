87878a9f01777eba45ae0d5897ed3734f64301f1
hanaisreal <okd2000@gmail.com>
2022-11-17 23:47:06 +0900

    dbsqlite added

--- frontend/cu/src/components/RatingForm/HeartRating.test.tsx
+++ frontend/cu/src/components/RatingForm/HeartRating.test.tsx
@@ -2,6 +2,7 @@ import { render, screen, fireEvent } from "@testing-library/react"
 import HeartRating from "./HeartRating"
 
 
+
 describe("<HeartRating />", () =>{
     beforeEach(() => {
         jest.clearAllMocks();
@@ -9,11 +10,25 @@ describe("<HeartRating />", () =>{
 
     it("should render without errors", () =>{
         const {container} = render(<HeartRating 
-            score={0}  
+            score={3}  
             updateScore={jest.fn()}
             />);
         const rating = container.getElementsByClassName("heart");
         expect(rating.length).toBe(5);  
-    
+    })
+
+    it("should handle click", () =>{
+        // const {container} = render(<HeartRating 
+        //     score={3}  
+        //     updateScore={jest.fn()}
+        //     />);
+        // const button = screen.getByTitle('heart');
+        // fireEvent.click(button);
+        // expect(button).toHaveAttribute('on');
+        // const countRate = container.getElementsByClassName("on");
+        // expect(countRate.length).toBe(3);
+
+
+
     })
 })
