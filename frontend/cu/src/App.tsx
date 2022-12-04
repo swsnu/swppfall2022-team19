c93040c1b118022bd48c7932a631a060c3ccad73
GAON25 <104995652+GAON25@users.noreply.github.com>
2022-12-05 03:02:46 +0900

    Update App.tsx

--- frontend/cu/src/App.tsx
+++ frontend/cu/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
       dispatch(getUsers()).then(() => dispatch(getRequestUser()));
       console.log("selectedUser: " + selectedUserState?.username);
     }
-  });
+  }, []);
 
 
   return (
