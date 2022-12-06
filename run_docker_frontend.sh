62f0feb6c0b7278eaeac075ca0ea5d5de62b05aa
yunwkgus <52576276+yunwkgus@users.noreply.github.com>
2022-12-06 22:44:45 +0900

    Update run_docker_frontend.sh

--- run_docker_frontend.sh
+++ run_docker_frontend.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker run -d -p 3000:3000 --rm --name "frontend" frontend:latest
+# sudo docker run -d -p 3000:3000 --rm --name "frontend" frontend:latest
+sudo docker run -d -p 80:80 --rm --name "frontend" frontend:latest
