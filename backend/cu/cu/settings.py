2bf1c98c8e5610215cb2b06f5cf8d2ffc455e3b9
yunwkgus <52576276+yunwkgus@users.noreply.github.com>
2022-12-06 17:41:25 +0900

    Update settings.py

--- backend/cu/cu/settings.py
+++ backend/cu/cu/settings.py
@@ -17,14 +17,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9v6cx(2nfme!d4e@x@h^t_#ahd6cr^y!d9i8xpjb&uz2p8ua@4'
+secret_key_default = 'LetmeCULetmeCULetmeCULetmeCULetmeCULetmeCULetmeCU!'
+SECRET_KEY = os.environ.get('SECRET_KEY', secret_key_default)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0', 'localhost', '13.125.244.234', 'ec2-13-125-244-234.ap-northeast-2.compute.amazonaws.com']
 #ALLOWED_HOSTS = ['*']
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')                      
+SECURE_HSTS_SECONDS = int(os.environ.get('SECURE_HSTS_SECONDS', 31536000))
+SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT', 'False') == 'True'                          # default: False
+SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', 'False') == 'True'                      # default: False
+CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', 'False') == 'True'                            # default: False
+SECURE_HSTS_INCLUDE_SUBDOMAINS = os.environ.get('SECURE_HSTS_INCLUDE_SUBDOMAINS', 'False') == 'True'    # default: False
+SECURE_HSTS_PRELOAD = os.environ.get('SECURE_HSTS_PRELOAD', 'False') == 'True'                          # default: False
 # Application definition
 
 INSTALLED_APPS = [
@@ -43,7 +51,7 @@ INSTALLED_APPS = [
 # ASGI_APPLICATION = 'cu.asgi.application'
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+#     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,7 +88,7 @@ WSGI_APPLICATION = 'cu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/db/db.sqlite3',
     }
 }
 
@@ -119,19 +127,53 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# for security
-CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8000/", "http://localhost"]
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000",
-                        "http://127.0.0.1:3000", "http://localhost"]
+# CSRF 
+CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = (
+     'http://ec2-13-125-244-234.ap-northeast-2.compute.amazonaws.com:3000',
+     'http://13.125.244.234:3000',
+     'https://ec2-13-125-244-234.ap-northeast-2.compute.amazonaws.com:3000',
+     'https://13.125.244.234:3000',
+     
+ )
+
+# CORS_ORIGIN_WHITELIST = (
+#    'http://ec2-13-124-241-236.ap-northeast-2.compute.amazonaws.com:3000',
+#     'http://13.124.241.236:3000',
+#     'https://ec2-13-124-241-236.ap-northeast-2.compute.amazonaws.com:3000',
+#     'https://13.124.241.236:3000',
+# )
+
+CORS_ALLOW_HEADERS = (
+    'access-control-allow-credentials',
+    'access-control-allow-origin',
+    'access-control-request-method',
+    'access-control-request-headers',
+    'accept',
+    'accept-encoding',
+    'accept-language',
+    'authorization',
+    'connection',
+    'content-type',
+    'dnt',
+    'credentials',
+    'host',
+    'origin',
+    'user-agent',
+    'X-CSRFToken',
+    'csrftoken',
+    'x-requested-with',
+)
 # Added - Custom User model
 AUTH_USER_MODEL = 'user.User'
 
@@ -142,7 +184,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #MEDIA_ROOT = os.path.join(os.path.
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = 'true'
 
 
-'''
+
 # pip install djangorestframework-simplejwt
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
@@ -154,6 +196,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+'''
 JWT_AUTH = {
     'JWT_SECRET_KEY': SECRET_KEY,
     'JWT_ALGORITHM': 'HS256',
@@ -161,4 +205,5 @@ JWT_AUTH = {
     # 'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
     # 'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=28),
 }
+B
 '''
