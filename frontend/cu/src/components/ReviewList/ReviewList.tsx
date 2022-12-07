f1df3e516be75390d074c46d97d1515b85a1d669
nyminna <lizzie00@snu.ac.kr>
2022-12-07 13:38:46 +0900

    css nayeong

--- frontend/cu/src/components/ReviewList/ReviewList.tsx
+++ frontend/cu/src/components/ReviewList/ReviewList.tsx
@@ -56,27 +56,36 @@ export default function ReviewList (props: Props){
 
     return (
         <div className="ReviewList"> 
-                
             <div className='title'>
                 상품후기( {filteredRates.length} )
-                <button id='button' className="likeButton" onClick={() =>likeButtonClick()}>인기순</button>
-                <button id='button' className="recentButton" onClick={() =>recentButtonClick()}>최신순</button>
-                <button id='button' className="pictureButton" onClick={() =>pictureButtonClick()}>사진리뷰</button>
+                <div className="sorting_buttons">
+                    <button>
+                        <a onClick={() =>likeButtonClick()}>인기순</a>
+                    </button>
+                    <a>|</a>
+                    <button>
+                        <a onClick={() =>recentButtonClick()}>최신순</a>
+                    </button>
+                    <a>|</a>
+                    <button>
+                        <a onClick={() =>pictureButtonClick()}>사진리뷰</a>
+                    </button>
+                </div>
             </div>
-            <div className='fieldName'>
-                <div className="fieldName_date">작성 날짜 </div>
-                <div className="fieldName_user">작성자 </div>
-                <div className="fieldName_totalScore">평점 </div>
-                <div className="fieldName_comment">한줄 평가 </div>
-                <div className="fieldName_like">좋아요 </div>
+            <div className='fields'>
+                <div className="field_date">작성 날짜 </div>
+                <div className="field_user">작성자 </div>
+                <div className="field_totalScore">평점 </div>
+                <div className="field_comment">한줄 평가 </div>
+                <div className="field_like">좋아요 </div>
             </div>    
             <div className='reviews'>
+                {  (filteredRates.length !== 0 && rates.length === 0 )&& <div className='no_picture_review'>첫 사진 리뷰를 남겨주세요</div> }
                 {  rates.reverse().map( (rv) => {
                     return (<Review key={rv.id} user={props.user} product={props.product} rate={rv}/>
                 );})}
-                { filteredRates.length===0 && <div className='no_review'>첫 평가를 남겨주세요</div>}
+                { filteredRates.length===0 && <div className='no_review'>첫 리뷰를 남겨주세요</div>}
             </div>
         </div> 
     )
 }
-
