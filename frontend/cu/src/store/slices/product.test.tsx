4fd8178aeb618d543afcefbbc42de60d5ea08c13
yunwkgus <52576276+yunwkgus@users.noreply.github.com>
2022-12-10 14:16:55 +0900

    Update product.test.tsx

--- frontend/cu/src/store/slices/product.test.tsx
+++ frontend/cu/src/store/slices/product.test.tsx
@@ -1,7 +1,6 @@
 import { EnhancedStore, ThunkMiddleware, AnyAction, configureStore } from '@reduxjs/toolkit'
-import { waitFor } from '@testing-library/react'
 import axios from 'axios'
-import reducer, { ProductState, fetchProduct, fetchQueryProducts, updateProduct, fetchSearchProducts, fetchAllProducts } from './product'
+import reducer, { ProductState, fetchProduct, fetchQueryProducts, fetchSearchProducts, fetchAllProducts } from './product'
 
 describe('product reducers', () => {
     let store: EnhancedStore<{ product: ProductState }, AnyAction, [ThunkMiddleware<{ product: ProductState }, AnyAction, undefined>]>
