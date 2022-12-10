ccf2e52b97554a4dac92f63d4da5d878d2d3d1c2
yunwkgus <52576276+yunwkgus@users.noreply.github.com>
2022-12-10 14:27:40 +0900

    Update User.ts

--- frontend/cu/src/store/slices/User.ts
+++ frontend/cu/src/store/slices/User.ts
@@ -1,256 +1,211 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-// 테스팅 각주 import axios from "axios";
-import client from '../api/client';
-import { RootState } from "..";
-
-// axios.defaults.xsrfCookieName = 'csrftoken';
-// axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-
-export interface UserType {
-  id: number;
-  username: string;
-  password: string;
-  gender: number;
-  age: number;
-  taste: string;
-  question: number;
-  loginState: boolean;
-}
-export interface UserLoginRequest {
-  username: string;
-  password: string;
-}
-export interface UserSignupRequest {
-  username: string;
-  password: string;
-  gender: number;
-  age: number;
-  taste: string;
-  question: number;
-}
+/ src/store/slices/User.test.ts
+ import { AnyAction, configureStore, EnhancedStore } from "@reduxjs/toolkit";
+ import { ThunkMiddleware } from "@reduxjs/toolkit";
 
 
-export interface SurveyRequest {
-  gender: number;
-  age: number;
-  taste: string;
-  question: number;
-}
+ import reducer, { UserState } from './User';
+ import { postUser, loginUser, signoutUser, getUsers, getRequestUser, putSurvey, getRequestUserAtLogin } from './User';
+ import client from '../api/client';
+
+ jest.mock("axios");
+
+ describe("User reducer", () => {
+     let store: EnhancedStore<{ userState: UserState }, AnyAction, [ThunkMiddleware<{ userState: UserState }, AnyAction, undefined>]>;
+     const fakeUser1 = { id: 1, 'username': 'fakeUser1', 'password': '12345', 'gender': 1, 'age': 1, 'taste': 'AB', 'question': 1, 'loginState': false };
+     const fakeUser1_loggedIn = { id: 1, 'username': 'fakeUser1', 'password': '12345', 'gender': 1, 'age': 1, 'taste': 'AB', 'question': 1, 'loginState': true };
+     const fakeUser2 = { id: 2, 'username': 'fakeUser2', 'password': '12345', 'gender': 2, 'age': 2, 'taste': 'ABC', 'question': 2, 'loginState': true };
+     const fakeUser3 = { id: 3, 'username': 'fakeUser3', 'password': '12345', 'gender': 1, 'age': 3, 'taste': 'DE', 'question': 3, 'loginState': false };
+
+     const wrongUserInput = { 'username': 'wrong', 'password': 'wrong' };
+     const fakeUserList1 = [fakeUser1, fakeUser2];
+     const fakeUserList2 = [fakeUser1, fakeUser2, fakeUser3];
+     const changeSurvey1 = { id: 1, 'gender': 1, 'age': 3, 'taste': 'C', 'question': 2 };
+     const changedFakeUser1 = { id: 1, 'username': 'fakeUser1', 'password': '12345', 'gender': 1, 'age': 3, 'taste': 'C', 'question': 2, 'loginState': true };
+
+     beforeAll(() => {
+         store = configureStore(
+             { reducer: { userState: reducer } }
+         );
+
+     });
 
 
-export interface UserState {
-  users: UserType[];
-  selectedUser: UserType | null;
-}
+     it("should handle initial State", () => {
+         expect(reducer(undefined, { type: "unknown" })).toEqual({
+             users: [],
+             selectedUser: null,
+         });
+     });
 
-const initialState: UserState = {
-  users: [],
-  selectedUser: null,
-};
+     it("should handle putSurvey - null", async () => {
+         jest.spyOn(client, "put").mockResolvedValue({
+             data: null
+         })
+         await store.dispatch(
+             putSurvey({ id: 1, 'gender': 1, 'age': 3, 'taste': 'C', 'question': 2 })
+         );
+         expect(store.getState().userState.selectedUser).toEqual(null);
+     });
 
+     it("should handle getUsers_no users(null)", async () => {
+         // const client = axios.create.mockReturnThis();
+         // client.get = jest.fn().mockResolvedValue({ data: null });
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: null
+         });
+         await store.dispatch(getUsers());
+         expect(store.getState().userState.users).toEqual([]);
+     });
 
+     it("should handle getUsers", async () => {
+         jest.spyOn(client, "get").mockResolvedValue({ data: fakeUserList1 })
 
+         // client.get = jest.fn().mockResolvedValue({ data: fakeUserList1 });
+         await store.dispatch(getUsers());
+         expect(store.getState().userState.users).toEqual(fakeUserList1);
+     });
 
-// 1. 회원가입 함수. 유저 정보 저장.
-export const postUser = createAsyncThunk( // signup
-  "user/postUser",
-  async (user: Pick<UserSignupRequest, "username" | "password" | "age" | "gender" | "question" | "taste">, { dispatch }) => {
-    const response = await client.post("api/user/signup/", user);
-
-    dispatch(userActions.addUser(response.data));
-  }
-);
-
-// 2. 로그인 함수. selectedUser 설정.
-export const loginUser = createAsyncThunk(
-  "user/loginUser",
-  async (user: Pick<UserLoginRequest, "username" | "password">, { dispatch }) => {
-    const response = await client.post("/api/user/signin/", user);
-    dispatch(userActions.loginUser(response.data));
-    // localStorage.setItem('loginUser', JSON.stringify(response.data));
-  }
-)
-
-// 3. 로그아웃 함수 
-export const signoutUser = createAsyncThunk(
-  "user/signoutUser",
-  async (user: void, { dispatch }) => {
-  
-    const response = await client.get("/api/user/signout/");
-
-    dispatch(userActions.signoutUser(response.data));
-    // 그런데 로그인의 경우 로그인 실패 시 에러가 발생할 수도 있음
-    // 에러 HttpResponse(status=401)인데, 로그인 오류 시 alert 
-    // localStorage.clear();
-
-  }
-)
-
-// 4. getUsers. 백엔드에서 userList 받아와서 initialState 업데이트
-export const getUsers = createAsyncThunk(
-  "user/getUsers",
-  async (user: void, { dispatch }) => {
-    const response = await client.get("/api/user/userlist/");
-    
-    dispatch(userActions.getUsers(response.data));
-    return response.data ?? null;
-  }
-)
-
-// 5. getRequestUser. 현재 request.user의 정보를 받아옴
-export const getRequestUser = createAsyncThunk(
-  "user/getRequestUser",
-  async (user: void, { dispatch }) => {
-    const response = await client.get("/api/user/requestUser/");
-
-
-    if (response.data === '') {
-      window.location.replace('/login');
-      // localStorage.clear();
-    } else {
-
-      // localStorage.setItem('loginUser', JSON.stringify(response.data));
-    }
-    dispatch(userActions.getRequestUser(response.data));
-    return response.data ?? null;
-  }
-)
-
-export const getRequestUserAtLogin = createAsyncThunk(
-  "user/getRequestUser",
-  async (user: void, { dispatch }) => {
-    const response = await client.get("/api/user/requestUser/");
-
-    if (response.data === '') {
-
-      // localStorage.clear();
-      // window.location.replace('/login');
-    } else {
-      // localStorage.setItem('loginUser', JSON.stringify(response.data));
-      window.location.replace('/home');
-    }
-    dispatch(userActions.getRequestUser(response.data));
-    return response.data ?? null;
-  }
-)
-
-
-export const putSurvey = createAsyncThunk(
-  "user/newSurvey",
-  async (user: Pick<UserType, "id" | "age" | "gender" | "taste" | "question">, { dispatch }) => {
-
-    const response = await client.put(`/api/user/newSurvey/${user.id}/`, user);
-
-    dispatch(userActions.putSurvey(response.data));
-  }
-)
-
-export const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    getRequestUser: (state, action: PayloadAction<UserType>) => {
-          if (action.payload === null || action.payload === undefined) {
-        state.selectedUser = null;
-       
-      } else {
-        const targetUser = state.users.find(
-          (user: UserType) => (user.username === action.payload.username)
-        );
-        if (targetUser) {
-          state.selectedUser = targetUser;
-        } else {
-        
-          state.selectedUser = null;
-          // localStorage.clear();
-          // window.location.replace('/login');
-        }
-
-
-      }
-    },
-    getUsers: (state, action: PayloadAction<UserType[]>) => {
-
-      if (action.payload === null || action.payload === undefined) {
-
-      } else {
-        var user_list: UserType[] = [];
-        for (var i = 0; i < action.payload.length; i++) {
-          user_list.push({ "id": action.payload[i].id, "username": action.payload[i].username, "password": action.payload[i].password, "age": action.payload[i].age, "gender": action.payload[i].gender, "loginState": action.payload[i].loginState, "taste": action.payload[i].taste, "question": action.payload[i].question });
-        }
-        // 유저탈퇴 기능이 있다면 오류가 생길 수 있음
-
-        state.users = user_list;
-   
-      }
-    },
-    loginUser: (state, action: PayloadAction<UserLoginRequest>) => {
-      const targetUser = state.users.find(
-        (user: UserType) => (user.username === action.payload.username)
-        // && (user.password === action.payload.password)
-      );
-
-      if (targetUser) {
-        targetUser.loginState = true;
-        state.selectedUser = targetUser;
-       
-      }
-      else {
-       
-        state.selectedUser = null;
-      };
-
-    },
-
-    signoutUser: (state, action: PayloadAction<UserLoginRequest>) => {
-
-      state.selectedUser!.loginState = false;
-      state.selectedUser = null;
-
-    },
-
-    addUser: ( // register, signup
-      state,
-      action: PayloadAction<UserType>
-    ) => {
-      // return { ...state, selectedUser: action}
-      const newUser = {
-        id: action.payload.id, // state.users[state.users.length - 1].id + 1, // temporary
-        username: action.payload.username,
-        password: action.payload.password,
-
-        gender: action.payload.gender,
-        age: action.payload.age,
-        taste: action.payload.taste,
-        question: action.payload.question,
-        loginState: false
-      };
-      state.users.push(newUser);
-
-    },
-    putSurvey: (state, action: PayloadAction<SurveyRequest>) => {
-
-      if (state.selectedUser == null) {
-
-      } else {
-        state.selectedUser.gender = action.payload.gender;
-        state.selectedUser.age = action.payload.age;
-        state.selectedUser.taste = action.payload.taste;
-        state.selectedUser.question = action.payload.question;
-
-        state.users[state.selectedUser.id] = state.selectedUser;
-
-      }
-    },
-  },
-
-});
-
-export const userActions = userSlice.actions;
-export const selectUser = (state: RootState) => state.user;
-export const userList = (state: RootState) => state.user.users;
-export const selectedUser = (state: RootState) => state.user.selectedUser;
-export default userSlice.reducer;
+     it("should handle putSurvey - null", async () => {
+         reducer(undefined, { type: "unknown" });
+         jest.spyOn(client, "put").mockResolvedValue({
+             data: changeSurvey1
+         })
+         await store.dispatch(
+             putSurvey({ id: 1, 'gender': 1, 'age': 3, 'taste': 'C', 'question': 2 })
+         );
+         expect(store.getState().userState.selectedUser).toEqual(null);
+     });
 
 
 
+
+     it("should handle getRequestUser_null", async () => {
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: null
+         })
+         await store.dispatch(getRequestUser());
+         expect(store.getState().userState.selectedUser).toEqual(null);
+     });
+     xit("should handle getRequestUser_\'\'", async () => {
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: ''
+         })
+         await store.dispatch(getRequestUser());
+         global.window = Object.create(window);
+         const url = "/login";
+         Object.defineProperty(window, 'location', {
+             value: {
+                 replace: url
+             }
+         });
+         expect(window.location.replace).toEqual(url);
+         expect(store.getState().userState.selectedUser).toEqual(null);
+     });
+     it("should handle getRequestUser_loggedIn", async () => {
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: fakeUser1
+         })
+         await store.dispatch(getRequestUser());
+         expect(store.getState().userState.selectedUser).toEqual(fakeUser1);
+     });
+     it("should handle getRequestUser_user", async () => {
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: wrongUserInput
+         })
+         await store.dispatch(getRequestUser());
+         expect(store.getState().userState.selectedUser).toEqual(null);
+     });
+
+     xit("should handle getRequestUserAtLogin_real user", async () => {
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: fakeUserList1
+         })
+         await store.dispatch(getUsers());
+
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: fakeUser1
+         })
+         await store.dispatch(getRequestUserAtLogin());
+         global.window = Object.create(window);
+         const url = "/home";
+         Object.defineProperty(window, 'location', {
+             value: {
+                 replace: url
+                 // href: url
+             }
+         });
+         // expect(window.location.replace).toEqual(url);
+         expect(store.getState().userState.selectedUser).toEqual(fakeUser1);
+     });
+     it("should handle getRequestUserAtLogin_undefined user", async () => {
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: ''
+         })
+         // axios.get = jest.fn().mockResolvedValue({  });
+         await store.dispatch(getRequestUserAtLogin());
+
+         expect(store.getState().userState.selectedUser).toEqual(null);
+     });
+
+
+
+     it("should handle postUser(signUp)", async () => {
+         jest.spyOn(client, "post").mockResolvedValue({
+             data: fakeUser3
+         });
+         await store.dispatch(
+             postUser({ 'username': 'fakeUser3', 'password': '12345', 'gender': 1, 'age': 3, 'taste': 'DE', 'question': 3 })
+         );
+         expect(store.getState().userState.users).toEqual(fakeUserList2);
+     });
+     it("should handle loginUser", async () => {
+         jest.spyOn(client, "post").mockResolvedValue({
+             data: fakeUser1_loggedIn
+         })
+         await store.dispatch(
+             loginUser({ 'username': 'fakeUser1', 'password': '12345' })
+         );
+         expect(store.getState().userState.selectedUser).toEqual(fakeUser1_loggedIn);
+         // expect(store.getState().todo.todos.find((v) => v.id === fakeTodo.id)?.done).toEqual(true);
+     });
+     it("should handle signoutUser", async () => {
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: null
+         });
+
+         await store.dispatch(
+             signoutUser()
+         );
+         expect(store.getState().userState.selectedUser).toEqual(null);
+         // expect(store.getState().todo.todos.find((v) => v.id === fakeTodo.id)?.done).toEqual(true);
+     });
+     it("should handle fail loginUser", async () => {
+         jest.spyOn(client, "post").mockResolvedValue({
+             data: wrongUserInput
+         });
+         await store.dispatch(
+             loginUser({ 'username': 'wrong', 'password': 'wrong' })
+         );
+         expect(store.getState().userState.selectedUser).toEqual(null);
+         // expect(store.getState().todo.todos.find((v) => v.id === fakeTodo.id)?.done).toEqual(true);
+     });
+
+
+     it("should handle putSurvey - not null", async () => {
+         jest.spyOn(client, "get").mockResolvedValue({
+             data: fakeUser1
+         })
+         await store.dispatch(getRequestUser());
+
+         jest.spyOn(client, "put").mockResolvedValue({
+             data: changeSurvey1
+         })
+         await store.dispatch(
+             putSurvey({ id: 1, 'gender': 1, 'age': 3, 'taste': 'C', 'question': 2 })
+         );
+
+         expect(store.getState().userState.selectedUser).toEqual(changedFakeUser1);
+     });
+
+
+ });
