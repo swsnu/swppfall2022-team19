419b4c62686cb334c72540c5d80f2b679bbcafa2
hanaisreal <okd2000@gmail.com>
2022-12-14 13:54:41 +0900

    trial2 edit setting

--- backend/cu/cu/settings.py
+++ backend/cu/cu/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': "",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4', # 테이블 생성 자동으로 해줄때 쓸 인코딩,, 이거안하면 밑에꺼해도 효과 엑스
             'use_unicode': True,
         },
